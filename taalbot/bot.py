6b3d3bff3b3fe5a39112a993264a04f07ea10149
Théo Piboubès <734711+ThePib@users.noreply.github.com>
2019-12-03 03:13:31 +0100

    [taalbot] Add a basic !dehet command

--- taalbot/bot.py
+++ taalbot/bot.py
@@ -1,7 +1,10 @@
 from discord.ext import commands
+from io import StringIO
 
 import discord
+import json
 import os
+import requests
 import sys
 
 
@@ -10,6 +13,34 @@ taalbot = commands.Bot(command_prefix='!')
 @taalbot.command()
 async def ping(ctx):
     await ctx.send('Pong!')
+
+@taalbot.command(name='dehet')
+async def search_word(ctx, word):
+    output_buf = StringIO()
+
+    response = requests.get("http://localhost:8000/api/v1/woorden/search/{}".format(word))
+    words = json.loads(response.text)
+
+    result_prefix = ""
+    if len(words) > 1:
+        output_buf.write("I found multiple words! 🔎\nHere are the first few ones:\n")
+        result_prefix = "- "
+
+    for obj in words:
+        articles = obj['lidwoord_name']
+        both_articles = len(articles) == 2
+        result_suffix = ""
+
+        if both_articles:
+            article_output = "**{}**/**{}**".format(*articles)
+            result_suffix = "⚠"
+        else:
+            article_output = "**{}**".format(*articles)
+
+        word = obj['woord']
+        output_buf.write("{}{} {} {}\n".format(result_prefix, article_output, word, result_suffix))
+
+    await ctx.send(output_buf.getvalue())
 
 if __name__ == '__main__':
     try:
