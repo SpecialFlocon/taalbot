b83e341b18b7998772f850eeed9632ad534b1044
Théo Piboubès <734711+ThePib@users.noreply.github.com>
2019-12-04 01:19:01 +0100

    [taalbot] Better error handling, receiving end

--- taalbot/bot.py
+++ taalbot/bot.py
@@ -19,6 +19,17 @@ async def search_word(ctx, word):
     output_buf = StringIO()
 
     response = requests.get("http://localhost:8000/api/v1/woorden/search/{}".format(word))
+    # HTTP 500: couldn't extract info from sources
+    # TODO(thepib): notify developer so that he can get on with fixing his code
+    if response.status_code == 500:
+        # Actually a lie (for now)
+        await ctx.send("Oops, the rocket blew up in-flight. I just harassed whoever wrote me about it, so you don't have to do it.")
+        return
+    # HTTP 404: word doesn't exist
+    if response.status_code == 404:
+        await ctx.send("The word you're looking for was not found.\nIs it a *zelfstandig naamwoord*? Is it even a word?")
+        return
+
     words = json.loads(response.text)
 
     result_prefix = ""
