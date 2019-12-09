02568ec02385851e7e4c5ff270dd25be77d0a559
Théo Piboubès <734711+ThePib@users.noreply.github.com>
2019-12-09 18:48:43 +0100

    Let !dehet command accept variable number of args

--- src/taalbot/cogs/lidwoord.py
+++ src/taalbot/cogs/lidwoord.py
@@ -9,8 +9,7 @@ class LidwoordCog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command(name='dehet')
-    async def search_word(self, ctx, word: str):
+    def search_word(self, word):
         output_buf = StringIO()
 
         response = requests.get("{}/api/{}/woorden/search/{}".format(self.bot.api_url, self.bot.api_version, word))
@@ -18,12 +17,10 @@ class LidwoordCog(commands.Cog):
         # TODO(thepib): notify developer so that he can get on with fixing his code
         if response.status_code == 500:
             # Actually a lie (for now)
-            await ctx.send("Oops, the rocket blew up in-flight. I just harassed whoever wrote me about it, so you don't have to do it.")
-            return
+            return "Oops, the rocket blew up in-flight. I just harassed whoever wrote me about it, so you don't have to do it."
         # HTTP 404: word doesn't exist
         if response.status_code == 404:
-            await ctx.send("The word you're looking for was not found.\nIs it a *zelfstandig naamwoord*? Is it even a word?")
-            return
+            return "The word you're looking for was not found.\nIs it a *zelfstandig naamwoord*? Is it even a word?"
 
         words = json.loads(response.text)
 
@@ -45,7 +42,14 @@ class LidwoordCog(commands.Cog):
             word = obj['woord']
             output_buf.write("{}{} {} {}\n".format(result_prefix, article_output, word, result_suffix))
 
-        await ctx.send(output_buf.getvalue())
+        return output_buf.getvalue()
+
+    @commands.command()
+    async def dehet(self, ctx, *args):
+        if len(args) == 1:
+            await ctx.send(search_word(word))
+        else:
+            await ctx.send_help(LidwoordCog)
 
 def setup(bot):
     bot.add_cog(LidwoordCog(bot))
