1b92a0ccb44b5b851408ddaa6352924d36d72bda
Théo Piboubès <734711+ThePib@users.noreply.github.com>
2019-12-10 02:44:46 +0100

    Add a method to add/update a word in taalapi's database

--- src/taalbot/cogs/lidwoord.py
+++ src/taalbot/cogs/lidwoord.py
@@ -9,6 +9,11 @@ class LidwoordCog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
+    # TODO(thepib): articles are not expected to change anytime soon, preload them somewhere global scope?
+    def get_articles(self):
+        response = requests.get("{}/api/{}/lidwoorden".format(self.bot.api_url, self.bot.api_version))
+        return json.loads(response.text)
+
     def get_or_learn_word(self, word):
         output_buf = StringIO()
 
@@ -36,6 +41,50 @@ class LidwoordCog(commands.Cog):
         output_buf.write("{} {} {}\n".format(article_output, word, result_suffix))
 
         return output_buf.getvalue()
+
+    def set_word(self, word, new_article):
+        """
+        Create a new word with given article,
+        or replace the article of an existing word.
+
+        Stalemate resolution algorithm:
+        - step 1: the word of the user who ran the command is final.
+        """
+
+        response = requests.get("{}/api/{}/woorden/search/{}".format(self.bot.api_url, self.bot.api_version, word))
+        # HTTP 404: word doesn't exist in taalapi's database, we'll create it later.
+        word_exists = response.status_code != 404
+
+        articles = self.get_articles()
+        if word_exists:
+            obj = json.loads(response.text)
+            if new_article == 'both':
+                obj['lidwoord'] = [a['id'] for a in articles]
+            else:
+                obj['lidwoord'] = [a['id'] for a in articles if a['lidwoord'] == new_article]
+
+            # Automatically mark new word/article combination as accurate.
+            obj['accurate'] = True
+
+            # Update word/article combination
+            response = requests.put("{}/api/{}/woorden/{}/".format(self.bot.api_url, self.bot.api_version, obj['id']), data=obj)
+            if response.status_code != requests.codes.ok:
+                return "I couldn't replace the article of this word. Not because I don't believe you, but because of a technical (or human) deficiency."
+
+            return "Article for word {} has been set to **{}**. Thank you for your valuable contribution!".format(word, new_article)
+        else:
+            new_word = dict()
+            new_word['woord'] = word
+            if new_article == 'both':
+                new_word['lidwoord'] = [a['id'] for a in articles]
+            else:
+                new_word['lidwoord'] = [a['id'] for a in articles if a['lidwoord'] == new_article]
+
+            response = requests.post("{}/api/{}/woorden/{}/".format(self.bot.api_url, self.bot.api_version), data=new_word)
+            if response.status_code != requests.codes.ok:
+                return "I couldn't add this word/article combination to my record. Not because I don't believe you, but because of a technical (or human) deficiency."
+
+            return "Word {} with article **{}** has been added to my record. Thank you for your valuable contribution!".format(word, new_article)
 
     @commands.command()
     async def dehet(self, ctx, *args):
