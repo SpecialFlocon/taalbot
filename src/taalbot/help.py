c414c9fad8b26e78fe5cefbc374042bac6a158fc
Théo Piboubès <734711+ThePib@users.noreply.github.com>
2019-12-11 03:41:33 +0100

    Implement most features of TaalbotHelpCommand

--- src/taalbot/help.py
+++ src/taalbot/help.py
@@ -1,16 +1,45 @@
-from discord.ext.commands import DefaultHelpCommand
+from discord.ext.commands import HelpCommand
+
+from .const import TAALBOT_CMD_BLUEPRINT
 
 import discord
 
 
-class TaalbotHelpCommand(DefaultHelpCommand):
+class TaalbotHelpCommand(HelpCommand):
+    def __init__(self, **options):
+        self.embed = options.pop('embed', discord.Embed())
+
+        super().__init__(**options)
+
     async def send_bot_help(self, mapping):
         ctx = self.context
         bot = ctx.bot
 
-        content = discord.Embed(title = 'Taalbot')
+        self.embed.title = 'Commands'
+        self.embed.description = """
+            Here's a list of the commands you can run, and their purpose.
+            Type `!help <command>` to get detailed help.
+        """
+        self.embed.colour = discord.Colour.blue()
 
-        if bot.description:
-            content.description = bot.description
+        filtered = await self.filter_commands(bot.commands, sort=True)
+        for c in filtered:
+            self.embed.add_field(
+                name='{}{}'.format(bot.command_prefix, c.name),
+                value=c.brief or 'No info',
+                inline=False,
+            )
 
-        await self.get_destination().send(embed=content)
+        await self.get_destination().send(embed=self.embed)
+
+    async def send_command_help(self, command):
+        try:
+            self.embed = discord.Embed.from_dict(TAALBOT_CMD_BLUEPRINT[command.name])
+        except KeyError:
+            return await self.send_error_message("I don't have a command called {}!".format(command.name))
+
+        await self.get_destination().send(embed=self.embed)
+
+    async def prepare_help_command(self, ctx, command=None):
+        self.embed = discord.Embed()
+        await super().prepare_help_command(ctx, command)
