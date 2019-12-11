b9d64433e461bdc4960f8881d1f2cbe58cda6187
Théo Piboubès <734711+ThePib@users.noreply.github.com>
2019-12-11 03:40:26 +0100

    Define a command map for HelpCommand formatting

--- src/taalbot/const.py
+++ src/taalbot/const.py
@@ -4,3 +4,29 @@ but might still be occasionally changed by hand.
 """
 
 API_VERSION = 'v1'
+TAALBOT_CMD_BLUEPRINT = {
+    'dehet': {
+        'title': "\"De\"-word or \"het\"-word?",
+        'description': "Which article does this word need? You could try to guess, or you could ask me!",
+        'color': 3447003, # 0x3498db, blue
+        'fields': [
+            {
+                'name': "Which article?",
+                'value': """
+                    `!dehet taal`
+                    `!dehet brood`
+                """,
+                'inline': False,
+            },
+            {
+                'name': "Set or replace article",
+                'value': """
+                    `!dehet jaar het`
+                    `!dehet thee de`
+                    `!dehet pad {}`
+                """.format('both'),
+                'inline': False,
+            }
+        ]
+    }
+}
