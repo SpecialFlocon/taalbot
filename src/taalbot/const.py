c39e010bc133a173ac8af9bd52e1de66ec520c70
Théo Piboubès <theo@piboubes.me>
2019-12-14 00:03:19 +0100

    Set API HTTP timeout to 3 seconds

--- src/taalbot/const.py
+++ src/taalbot/const.py
@@ -3,5 +3,5 @@ This file contains values that will never change at runtime,
 but might still be occasionally changed by hand.
 """
 
-API_REQUEST_TIMEOUT = 5 # seconds
+API_REQUEST_TIMEOUT = 3 # seconds
 API_VERSION = 'v1'
