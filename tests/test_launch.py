4ea9a9c0b87723ad041ab38c8ea69b9fc258d6f8
Théo Piboubès <theo@piboubes.me>
2019-12-14 17:05:14 +0100

    Add configuration tests

--- tests/test_launch.py
+++ tests/test_launch.py
@@ -1,7 +1,14 @@
 from src import taalbot
+from src.taalbot.exceptions import MissingConfigKeyError
 
 import pytest
 
+
+working_config = """
+apiUrl: http://api.url
+commandPrefix: "?"
+guildId: 1
+"""
 
 class TestLaunch:
     def test_exit_on_no_token(self):
@@ -21,3 +28,72 @@ class TestLaunch:
         args = ['-c', '/invalid/path']
         with pytest.raises(SystemExit):
             taalbot.main(args, test=True)
+
+    def test_missing_one_required_parameter(self, monkeypatch, tmp_path):
+        monkeypatch.setenv("TAALTOOL_BOT_TOKEN", "test")
+        mock_config = """
+commandPrefix: "?"
+guildId: 1
+"""
+        f = tmp_path / "config.yaml"
+        f.write_text(mock_config)
+        args = ['-c', str(f)]
+        with pytest.raises(MissingConfigKeyError):
+            taalbot.main(args, test=True)
+
+        f.unlink()
+        mock_config = """
+apiUrl: http://api.url
+guildId: 1
+"""
+
+        f.write_text(mock_config)
+        with pytest.raises(MissingConfigKeyError):
+            taalbot.main(args, test=True)
+
+        f.unlink()
+        mock_config = """
+apiUrl: http://api.url
+commandPrefix: "?"
+"""
+
+        f.write_text(mock_config)
+        with pytest.raises(MissingConfigKeyError):
+            taalbot.main(args, test=True)
+
+    def test_missing_two_required_parameters(self, monkeypatch, tmp_path):
+        monkeypatch.setenv("TAALTOOL_BOT_TOKEN", "test")
+        mock_config = """
+guildId: 1
+"""
+        f = tmp_path / "config.yaml"
+        f.write_text(mock_config)
+        args = ['-c', str(f)]
+        with pytest.raises(MissingConfigKeyError):
+            taalbot.main(args, test=True)
+
+        f.unlink()
+        mock_config = """
+commandPrefix: "?"
+"""
+
+        f.write_text(mock_config)
+        with pytest.raises(MissingConfigKeyError):
+            taalbot.main(args, test=True)
+
+        f.unlink()
+        mock_config = """
+apiUrl: http://api.url
+"""
+
+        f.write_text(mock_config)
+        with pytest.raises(MissingConfigKeyError):
+            taalbot.main(args, test=True)
+
+    def test_working_config(self, monkeypatch, tmp_path):
+        monkeypatch.setenv("TAALTOOL_BOT_TOKEN", "test")
+        mock_config = working_config
+        f = tmp_path / "config.yaml"
+        f.write_text(mock_config)
+        args = ['-c', str(f)]
+        assert taalbot.main(args, test=True)
