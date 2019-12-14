11d08247b0f17ceb1ff7969d91ad769013886d11
Théo Piboubès <theo@piboubes.me>
2019-12-14 22:18:49 +0100

    Add basic tests for API calls

---
+++ tests/test_lidwoord.py
@@ -0,0 +1,91 @@
+from src import taalbot
+from src.taalbot.cogs.lidwoord import LidwoordCog
+
+import pytest
+import requests
+
+
+class TestLidwoord:
+    """
+    This class contains tests for HTTP requests to taalapi.
+    """
+
+    def test_get_article_success(self, monkeypatch):
+        class MockResponse:
+            @staticmethod
+            def json():
+                return {'id': 1, 'key': "value"}
+
+            @staticmethod
+            def raise_for_status():
+                pass
+
+        def mock_get(self, *args, **kwargs):
+            return MockResponse()
+
+        cfg = {'apiUrl': "http://api.url", 'commandPrefix': "?", 'guildId': 1}
+        t = taalbot.Taalbot(cfg)
+
+        monkeypatch.setattr(requests, "get", mock_get)
+        result = LidwoordCog(t).get_articles()
+        assert result['id'] == 1 and result['key'] == "value"
+
+    def test_get_article_failure(self, monkeypatch):
+        class MockResponse:
+            @staticmethod
+            def json():
+                return None
+
+            @staticmethod
+            def raise_for_status():
+                raise requests.HTTPError
+
+        def mock_get(self, *args, **kwargs):
+            return MockResponse()
+
+        cfg = {'apiUrl': "http://api.url", 'commandPrefix': "?", 'guildId': 1}
+        t = taalbot.Taalbot(cfg)
+
+        monkeypatch.setattr(requests, "get", mock_get)
+        with pytest.raises(requests.HTTPError):
+            LidwoordCog(t).get_articles()
+
+    def test_get_word_success(self, monkeypatch):
+        class MockResponse:
+            @staticmethod
+            def json():
+                return {'id': 1, 'word': "word", 'article': "article"}
+
+            @staticmethod
+            def raise_for_status():
+                pass
+
+        def mock_get(self, *args, **kwargs):
+            return MockResponse()
+
+        cfg = {'apiUrl': "http://api.url", 'commandPrefix': "?", 'guildId': 1}
+        t = taalbot.Taalbot(cfg)
+
+        monkeypatch.setattr(requests, "get", mock_get)
+        result = LidwoordCog(t).get_or_learn_word('word')
+        assert result['id'] == 1 and result['word'] == "word"
+
+    def test_get_word_failure(self, monkeypatch):
+        class MockResponse:
+            @staticmethod
+            def json():
+                return None
+
+            @staticmethod
+            def raise_for_status():
+                raise requests.HTTPError
+
+        def mock_get(self, *args, **kwargs):
+            return MockResponse()
+
+        cfg = {'apiUrl': "http://api.url", 'commandPrefix': "?", 'guildId': 1}
+        t = taalbot.Taalbot(cfg)
+
+        monkeypatch.setattr(requests, "get", mock_get)
+        with pytest.raises(requests.HTTPError):
+            LidwoordCog(t).get_or_learn_word('word')
