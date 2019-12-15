bb2ddd099efe3e80332b3614b0449845611b60d3
Théo Piboubès <theo@piboubes.me>
2019-12-15 01:46:22 +0100

    Add cog for automated on-boarding process

---
+++ src/taalbot/cogs/onboarding.py
@@ -0,0 +1,47 @@
+from discord.ext import commands
+
+import discord
+import logging
+
+
+class Onboarding(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    async def dm_instructions(self, member):
+        await member.send("Onboarding process start! And done.")
+
+    @commands.Cog.listener()
+    async def on_member_join(self, member):
+        await self.dm_instructions(member)
+
+    @commands.command()
+    @commands.has_any_role('Onboarder')
+    @commands.has_permissions(administrator=True)
+    async def onboard(self, ctx, *, member: discord.Member=None):
+        member = member or ctx.author
+        logging.info("Onboarding for user {} has been requested.".format(member))
+        await self.dm_instructions(member)
+
+    @onboard.error
+    async def onboard_error(self, ctx, error):
+        author = ctx.message.author
+        if isinstance(error, commands.MissingPermissions) or isinstance(error, commands.MissingAnyRole):
+            await ctx.send(_("{}, you are not allowed to run this command. If you'd like to go through on-boarding process again, ask someone with enough permissions to initiate it for you.").format(author.mention))
+            if ctx.bot.log_channel:
+                await ctx.bot.log_channel.send("User {}, with insufficient permissions, just attempted to manually trigger the on-boarding process.".format(author.mention))
+
+            logging.info("Onboarding command invoked by user {} with insufficient permissions".format(author.name))
+            logging.debug("Permissions for user {}: {}".format(author.name, ctx.channel.permissions_for(author)))
+        else:
+            if ctx.bot.log_channel:
+                await self.log_channel.send("""
+**Error report**
+```
+{}
+```
+""".format(error))
+            logging.error(traceback.format_exception(type(error), error, error.__traceback__))
+
+def setup(bot):
+    bot.add_cog(Onboarding(bot))
