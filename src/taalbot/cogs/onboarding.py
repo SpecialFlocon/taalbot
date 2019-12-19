9f46bc28504ae8d4885f7c10dd04f0ae0abba725
Théo Piboubès <734711+ThePib@users.noreply.github.com>
2019-12-19 23:55:03 +0100

    Specify abbreviations for VK and VS on country step

--- src/taalbot/cogs/onboarding.py
+++ src/taalbot/cogs/onboarding.py
@@ -228,6 +228,7 @@ For this time only, I am asking you to *type* the country name (in Dutch!)
 Send ⏩ to skip this step.
 
 Example: **Nederland**
+Note: you must use **VK** for Verenigd Koninkrijk, and **VS** for Verenigde Staten!
 
 If you don't know what yours is, here is a list of country names in Dutch:
 https://www.101languages.net/dutch/country-names-dutch/
