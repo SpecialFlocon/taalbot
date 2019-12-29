7a29d506e36b1039feb490428ecdd5d405b86f4a
Théo Piboubès <theo@piboubes.me>
2019-12-29 01:56:18 +0100

    Defer translations in const.py

--- src/taalbot/const.py
+++ src/taalbot/const.py
@@ -3,6 +3,8 @@ This file contains values that will never change at runtime,
 but might still be occasionally changed by hand.
 """
 
+def _(m): return m
+
 API_REQUEST_TIMEOUT = 3 # seconds
 API_VERSION = 'v1'
 
