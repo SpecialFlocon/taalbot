de4fa1a0d822911a2c5f043675ed620482ce6c2b
Théo Piboubès <theo@piboubes.me>
2019-12-30 19:50:00 +0100

    Wrap URLs to prevent auto-preview

--- src/taalbot/cogs/shortcuts.py
+++ src/taalbot/cogs/shortcuts.py
@@ -13,11 +13,11 @@ class Shortcuts(commands.Cog):
 
     @commands.command(aliases=['betekenis'], brief=_("Search for a word in Van Dale free online dictionary"))
     async def vandale(self, ctx, word):
-        await ctx.send("{}/{}".format(const.VANDALE_SEARCH_BASE_URL, word))
+        await ctx.send("<{}/{}>".format(const.VANDALE_SEARCH_BASE_URL, word))
 
     @commands.command(aliases=['uitspraak'], brief=_("Listen to natives pronouncing a certain word"))
     async def forvo(self, ctx, word):
-        await ctx.send("{}/{}/nl/".format(const.FORVO_SEARCH_BASE_URL, word))
+        await ctx.send("<{}/{}/nl/>".format(const.FORVO_SEARCH_BASE_URL, word))
 
 def setup(bot):
     bot.add_cog(Shortcuts(bot))
